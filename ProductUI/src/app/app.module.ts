981d8dd83610355ea0aac9dd7bc2212d43d35286
kanishkkalra <kanishkk40@gmail.com>
2020-11-30 14:41:39 +0530

    footer added

--- ProductUI/src/app/app.module.ts
+++ ProductUI/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { CreateproductComponent } from './createproduct/createproduct.component';
 import { UpdateproductComponent } from './updateproduct/updateproduct.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListproductsComponent,
     CreateproductComponent,
-    UpdateproductComponent
+    UpdateproductComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
